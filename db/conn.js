31e6ba2a01caba72bef33ba159b37ab355603b7d
Thiago Henrique <tiago@DESKTOP-K3EL02S.localdomain>
2023-01-10 08:44:33 -0300

    Page Home -me

--- db/conn.js
+++ db/conn.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize('toughts', 'root', 'mysql', {
-    host: '172.18.65.118',
+    host: '172.18.78.99',
     dialect: 'mysql'
 })
 
